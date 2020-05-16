03637fdcad5e8c410867a5a4d12cfcdda9b95fe5
Leonardo Ramos <leo.ramos.ari@gmail.com>
2020-05-16 16:32:04 -0500

    se creo el error 404 y usamos prefech y goto

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,34 @@
+<script context="module">
+  export async function preload() {
+    let data = {};
+    const response = await this.fetch(
+      `https://us-central1-pugstagram-co.cloudfunctions.net/data`
+    );
+    data = await response.json();
+    return { data };
+  }
+</script>
+
 <script>
-  import { onMount } from "svelte";
+  /* se elimina por agregar el modulo import { onMount } from "svelte"; */
   import Header from "../components/Header.svelte";
   import Main from "../components/Main.svelte";
   import Sidebar from "../components/Sidebar.svelte";
   import TimeLine from "../components/TimeLine.svelte";
+  export let data;
 
-  let data = {};
+  /* se elimina por agregar el modulo  let data = {};
   const API = "https://us-central1-pugstagram-co.cloudfunctions.net/data";
 
   onMount(async () => {
     const response = await fetch(API);
     data = await response.json();
-  });
+  }); */
 </script>
+
+<svelte:head>
+  <title>Pugstagram</title>
+</svelte:head>
 
 <Main>
   <TimeLine posts={data.posts} />
